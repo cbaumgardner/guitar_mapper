62e536d52d7003e5e233bd833d28f6a6c37068e3
Cameron Baumgardner <31215393+cbaumgardner@users.noreply.github.com>
2022-03-07 18:19:23 -0500

    Adding gunicorn

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
-    flask_app = create_app()
-    flask_app.run(debug=True, host="0.0.0.0", port=4545)
+app = create_app()
+#if __name__ == '__main__':
+    # flask_app = create_app()
+    # flask_app.run(debug=True, host="0.0.0.0", port=4545)
