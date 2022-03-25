2add15621791561d80650a8d490913b0c3573920
Cameron Baumgardner <31215393+cbaumgardner@users.noreply.github.com>
2022-03-25 15:11:21 -0400

    Adding Pentatonic scales

--- scale_chart.py
+++ scale_chart.py
@@ -3,11 +3,15 @@ import math
 # These should probably go in a config file or something
 scale_types = [{"type": "Major (Ionian)", "scale_steps": [2, 2, 1, 2, 2, 2, 1]},
                {"type": "Minor (Aeolian)", "scale_steps": [2, 1, 2, 2, 1, 2, 2]},
+               {"type": "Major Pentatonic", "scale_steps": [2, 2, 3, 2, 3]},
+               {"type": "Minor Pentatonic", "scale_steps": [3, 2, 2, 3, 2]},
                {"type": "Dorian", "scale_steps": [2, 1, 2, 2, 2, 1, 2]},
                {"type": "Phrygian", "scale_steps": [1, 2, 2, 2, 1, 2, 2]},
                {"type": "Lydian", "scale_steps": [2, 2, 2, 1, 2, 2, 1]},
                {"type": "Mixolydian", "scale_steps": [2, 2, 1, 2, 2, 1, 2]},
-               {"type": "Locrian", "scale_steps": [1, 2, 2, 1, 2, 2, 2]}]
+               {"type": "Locrian", "scale_steps": [1, 2, 2, 1, 2, 2, 2]},
+               {"type": "Major Pentatonic", "scale_steps": [2, 2, 2, 2, 2]},
+               {"type": "Minor Pentatonic", "scale_steps": [2, 2, 2, 2, 2]}]
 key_notes = [{"key": "A", "key_display": "A"},
              {"key": "A#", "key_display": "A♯ / B♭"},
              {"key": "B", "key_display": "B"},
