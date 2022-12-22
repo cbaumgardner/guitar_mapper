2ebde8e3effd5c45a5e8b194c2fc850b480159f4
Cameron Baumgardner <31215393+cbaumgardner@users.noreply.github.com>
2022-12-22 13:08:38 -0500

    Adding docker stop to script

--- docker_restart.py
+++ docker_restart.py
@@ -1,5 +1,6 @@
 import os
 
+os.system("docker stop scale_chart")
 os.system("docker image rm scale_chart")
 os.system("docker build -t scale_chart .")
 os.system("docker run --rm -p 4545:4545 --name scale_chart scale_chart")
